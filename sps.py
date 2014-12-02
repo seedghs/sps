28a4aa952455af238dfc19c3fe6ba478e461f75f
Christopher Lin <christopher.lin045@gmail.com>
2014-12-02 13:30:45 -0600

    added table of contents, added prose, but prose is broken

--- sps.py
+++ sps.py
@@ -7,13 +7,23 @@ def writeFromTemplate(template,args=[]):
            for arg in args:
                if arg[0] in line:
                    render.write(line.replace(arg[0],arg[1]))
+def generateTable(content):
+    content.sort(key=lambda x: x[1])
+    table=[]
+    for contentBlock in content:
+        table.append("<a href='#"+str(contentBlock[0])+"'>" +str(contentBlock[2])+" <span style='font-style: italic'>by " + str(contentBlock[3]) + "</span></a><br>") 
+    return table
+
 def renderFile(content):
     for tline in template:
         if not "%" in tline:
             render.write(tline)
+        if tline == "%TABLEOFCONTENTS%\n":
+            for tableline in generateTable(content):
+                render.write(tableline)
         if(tline == "%CONTENT%\n"):
             for contentBlock in content:
-                render.write("<div class='contentcontainer'>\n")
+                render.write("<div class='contentcontainer' id='"+str(contentBlock[0])+"'>\n")
                 titleTemplate = open("titletemplate.html","r")
                 writeFromTemplate(titleTemplate,[["%TITLE%",contentBlock[2]],["%AUTHOR%",contentBlock[3]]])
                 titleTemplate.close()
@@ -22,6 +32,10 @@ def renderFile(content):
                     poemTemplate = open("poemtemplate.html","r")
                     writeFromTemplate(poemTemplate,[["%POEM%",contentBlock[4]]])
                     poemTemplate.close()
+                if contentBlock[1] == "prose":
+                    proseTemplate = open("prosetemplate.html","r")
+                    writeFromTemplate(proseTemplate,[["%PROSE%",contentBlock[4]]])
+                    proseTemplate.close()
                 render.write("</div>\n")
                 render.write("<br>")
 
